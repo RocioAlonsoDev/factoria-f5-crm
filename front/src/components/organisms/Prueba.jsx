b7c6c4970db94c8c70828e32c8ff285284ad8d51
AmaiiaAbaroa <abaroa.amaia@gmail.com>
2023-09-27 10:56:11 +0200

    dependencies installed and filles created

---
+++ front/src/components/organisms/Prueba.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+
+const Prueba= () => {
+  return (
+    <div >
+        <h1 className="max-w-6xl ml-1 font-extrabold">Hola FactoriaF5</h1>
+    </div>
+  );
+};
+
+
+export default Prueba;
