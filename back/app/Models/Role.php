e233edbbd478f79029b4ed2fbef7e07753398fad
SoleGrobas <solegrobas@yahoo.com.ar>
2023-09-28 12:15:48 +0200

    Role controler, model, rute OK

--- back/app/Models/Role.php
+++ back/app/Models/Role.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+    protected $hidden = [];
+
+    protected $fillable = [
+        'name',
+        
+       
+    ];
 }
--- back/routes/api.php
+++ back/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Role;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/role', Role::class);
