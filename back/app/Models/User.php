9a6df6374eca8e166452a121a48d3e643966b851
Rocío Alonso <130401333+RocioAlonsoDev@users.noreply.github.com>
2023-10-04 09:54:15 +0200

    Update User.php

--- back/app/Models/User.php
+++ back/app/Models/User.php
@@ -46,10 +46,10 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function role()
-    {
-        return $this->belongsTo(Role::class);
-    }
+    // public function role()
+    // {
+    //     return $this->belongsTo(Role::class);
+    // }
 
    
 }
