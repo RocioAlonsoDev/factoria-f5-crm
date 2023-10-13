945282f1216bd5859664692d93bb097cc2286c8c
anaencinasd <ana.encinasdiaz@gmail.com>
2023-10-13 15:12:52 +0200

    fixed things in form person

--- front/src/pages/Person/NavigationButtons.jsx
+++ front/src/pages/Person/NavigationButtons.jsx
@@ -17,7 +17,7 @@ export default function NavigationButtons({ onPrevious, onNext, section, totalSe
           onClick={onPrevious}
           style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
             <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15m0 0l6.75 6.75M4.5 12l6.75-6.75" />
           </svg>
           Atrás
