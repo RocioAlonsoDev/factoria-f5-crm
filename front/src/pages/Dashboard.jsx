fecfbff1ba31ff4ab88bb7e7ab12dce176a73f03
SoleGrobas <solegrobas@yahoo.com.ar>
2023-10-16 16:38:06 +0200

    setting views to adjust to outlet

--- front/src/pages/Dashboard.jsx
+++ front/src/pages/Dashboard.jsx
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
-=======
 import Sidebar from "../components/atoms/SideBarAtom";
 
->>>>>>> 94c356d5b8390dd208b1272517ba9a990150a05b
 
 export default function Dashboard() {
 
     return (
-<<<<<<< HEAD
-      <div>Dashboard</div>
-=======
       <><div><Sidebar></Sidebar></div></>
 
       
 
->>>>>>> 94c356d5b8390dd208b1272517ba9a990150a05b
     )
 }
--- front/src/pages/SelectionDay/SelectionDayIndex.jsx
+++ front/src/pages/SelectionDay/SelectionDayIndex.jsx
@@ -54,7 +54,7 @@ export default function CardAtomSocialTraffic() {
   });
 
   return (
-    <>
+    <div className='md:block md:fixed md:top-16 md:left-64 md:right-0 w-auto p-2'>
       <div className="flex flex-col items-center">
         <Link to="/recruitment/selectionday/add">
           <button className="bg-orange-500 text-white px-4 py-2 rounded mt-4">
@@ -98,6 +98,6 @@ export default function CardAtomSocialTraffic() {
         data={formattedPreviousSelections}
         columns={columns}
       />
-    </>
+    </div>
   );
 }
