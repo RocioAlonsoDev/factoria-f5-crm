680daebb9fb1f53c7b1bfcc928773abb7dbfadd8
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-10-17 11:31:09 +0200

    mailable and view for Discarded Status done

---
+++ back/resources/views/emails/discarded.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email</title>
+    <style>
+        h1 {
+            color: orangered;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>Hola {{ $name }}</h1>
+    <p>Lamentamos comunicarte que no has sido seleccionada para esta nueva edición en nuestros Bootcamps.</p>
+    
+</body>
+</html>
