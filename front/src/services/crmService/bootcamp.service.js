a5a67e5bc136eea994710a334dfc0688e3ce3da8
AmaiiaAbaroa <abaroa.amaia@gmail.com>
2023-10-17 21:56:09 +0200

    bootcamp.service created and profileError added

---
+++ front/src/services/crmService/bootcamp.service.js
@@ -0,0 +1,33 @@
+import http from "./http-common";
+
+class BootcampDataService{
+    getAll(){
+        return http.get("api/bootcamp");
+    }
+    get(id){
+        return http.get(`api/bootcamp/${id}`);
+    }
+    create(data){
+        
+        return http.post("api/bootcamp", data);
+        
+    }
+    update(id, data){
+        return http.put(`api/bootcamp/${id}`, data);
+    }
+    delete(id){
+        return http.delete(`api/bootcamp/${id}`);
+    }
+
+    deleteAll(){
+        return http.delete(`api/bootcamp`);
+    }
+
+    findByPerson(id_person){
+        return http.get(`api/bootcamp?id_person=${id_person}`);
+    }
+    
+   
+}
+
+export default new BootcampDataService();
