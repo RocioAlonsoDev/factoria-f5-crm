08ecf14b25c7c04fad2f310294d5cf131f29956e
LilinaG <lgutierrezfer@gmail.com>
2023-10-18 19:24:44 +0200

    dashboard table for selection days in progress

--- front/src/components/atoms/TableDashboardAtom.jsx
+++ front/src/components/atoms/TableDashboardAtom.jsx
@@ -3,22 +3,39 @@ import SelectionDayDataService from "../../services/recruitmentService/selection
 
 export default function TableDashboardAtom() {
   const [nextSelections, setNextSelections] = useState([]);
+  const [convocados, setConvocados] = useState({});
 
-  useEffect(() => {
-   
+useEffect(() => {
     SelectionDayDataService.getAll()
       .then((response) => {
-       
         const sortedSelections = response.data.data.sort((a, b) => {
           return new Date(a.date) - new Date(b.date);
         });
         setNextSelections(sortedSelections);
-        console.log(response.data);
+  
+        // Calcular el número de personas convocadas para cada SelectionDay
+        const convocadosData = {};
+        sortedSelections.forEach((selection) => {
+          const selectionDayId = selection.id;
+  
+          SelectionDayDataService.getPeopleInSelectionDay(selectionDayId)
+            .then((response) => {
+              const convocadosCount = response.data.data.length;
+              convocadosData[selectionDayId] = convocadosCount;
+  
+              // Actualizar el estado de convocados
+              setConvocados({ ...convocadosData });
+            })
+            .catch((error) => {
+              console.error("Error al obtener el número de personas convocadas", error);
+            });
+        });
       })
       .catch((error) => {
         console.error("Error al mostrar las jornadas de selección", error);
       });
   }, []);
+
   return (
     <>
       <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
@@ -50,39 +67,41 @@ export default function TableDashboardAtom() {
                   Fecha
                 </th>
                 <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                  Aspirantes
+                  Convocad@s
                 </th>
-                <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                {/* <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                   Confirmad@s
-                </th>
+                </th> */}
               </tr>
             </thead>
 
             <tbody>
-              {nextSelections.length > 0 ? (
-                nextSelections.map((selection, index) => (
-                  <tr key={index}>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                      {selection.school}
-                    </td>
-                    <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                      {new Date(selection.date).toLocaleDateString("es-ES", {
-                        day: "numeric",
-                        month: "numeric",
-                        year: "numeric",
-                      })}
-                    </td>
-                  </tr>
 
-                  
-                ))
-              ) : (
-                <tr>
-                  <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4" colSpan="2">
-                    No hay próximas jornadas de selección disponibles.
-                  </td>
-                </tr>
-              )}
+            {nextSelections.length > 0 ? (
+            nextSelections.map((selection, index) => (
+            <tr key={index}>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                {selection.school}
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                {new Date(selection.date).toLocaleDateString("es-ES", {
+                    day: "numeric",
+                    month: "numeric",
+                    year: "numeric",
+                })}
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                {convocados[selection.id] || 0} {/* Mostrar el número de convocados */}
+                </td>
+            </tr>
+        ))
+        ) : (
+            <tr>
+            <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4" colSpan="3">
+            No hay próximas jornadas de selección disponibles.
+            </td>
+            </tr>
+            )}
             </tbody>
           </table>
         </div>
