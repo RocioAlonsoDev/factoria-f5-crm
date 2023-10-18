4fa2dc3ae7ab16c086269eaca08567b409fad07d
anaencinasd <ana.encinasdiaz@gmail.com>
2023-10-18 08:51:21 +0200

    before merge. Login is not workiing

--- front/src/layouts/DefaultLayoutRecruitment.jsx
+++ front/src/layouts/DefaultLayoutRecruitment.jsx
@@ -17,9 +17,9 @@ export default function DefaultLayoutRecruitment({title}) {
     { label: 'Comentarios', url: '/recruitment/comments' },
   ];
   
-  if(!userToken){
-    return <Navigate to ='/login' />
-  }
+  // if(!userToken){
+  //   return <Navigate to ='/login' />
+  // }
 
   return (
     <>
