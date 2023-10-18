025584ab13405200fa8cb6251e997a2452c45b6c
AmaiiaAbaroa <abaroa.amaia@gmail.com>
2023-10-18 17:55:59 +0200

    user.image seeder added, comments deleted and  prop-types added

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Admin',
             'surname' => 'Admin', 
             'email' => 'admin@gmail.com',
+            'image' => 'https://i.pinimg.com/564x/ca/40/cd/ca40cdf8bde9af7fe54d85c0dec73e27.jpg', 
             'password' => bcrypt('123456Aa*'), 
             'id_role' => 1,
      ]);
@@ -32,9 +33,36 @@ class DatabaseSeeder extends Seeder
             'name' => 'Ana',
             'surname' => 'Alvarez', 
             'email' => 'ana@gmail.com',
-            'password' => '123456Aa*', 
-            'id_role' => 1,
+            'image' => 'https://img.freepik.com/fotos-premium/lindo-gato-camisa-floral-gafas-sol-sentado-aislado-sobre-fondo-blanco_962751-481.jpg',
+            'password' => bcrypt('123456Aa*'), 
+            'id_role' => 2,
      ]);
+        DB::table('users')->insert([
+            'name' => 'Cristina',
+            'surname' => 'Fernández', 
+            'email' => 'cristina@gmail.com',
+            'image' => 'https://img.freepik.com/fotos-premium/there-is-a-cat-that-is-sitting-on-a-rock-in-a-garden-generative-ai_900396-35758.jpg',
+            'password' => bcrypt('123456Aa*'), 
+            'id_role' => 2,
+     ]);
+        DB::table('users')->insert([
+            'name' => 'Manuela',
+            'surname' => 'Grajales', 
+            'email' => 'manuela@gmail.com',
+            'image' => 'https://img.freepik.com/fotos-premium/gato-traje-azul-dragon_662214-41149.jpg',
+            'password' => bcrypt('123456Aa*'), 
+            'id_role' => 3,
+     ]);
+        DB::table('users')->insert([
+            'name' => 'Maria',
+            'surname' => 'Villaverde', 
+            'email' => 'maria@gmail.com',
+            'image' => 'https://img.freepik.com/fotos-premium/ilustracion-gata-como-shaman-levantandose-humo_530527-29.jpg',
+            'password' => bcrypt('123456Aa*'), 
+            'id_role' => 3,
+     ]);
+
+
      $this->call(StatusSeeder::class);
      $this->call(RoleSeeder::class);
      $this->call(BootcampSeeder::class);
