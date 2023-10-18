e3315540c6a267e03c4d6eb38f094ee5309fdefe
BeatrizCano <beatrizcafernandez@gmail.com>
2023-10-18 12:10:18 +0200

    seeder_status_ok

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -20,7 +20,30 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-       
+        //$faker = FakerFactory::create(); // Crea una instancia de Faker
+
+        // Crea los estados primero
+        //Status::factory()->count(4)->create();
+
+        // Crea las bootcamps
+        //Bootcamp::factory()->count(5)->create();
+
+        // // Obtiene los IDs de los estados y las bootcamps
+        // $statusIds = Status::pluck('id')->toArray();
+        // $bootcampIds = Bootcamp::pluck('id')->toArray();
+
+        // // Crea las personas
+        // Person::factory()
+        //     ->count(10)
+        //     ->create([
+        //         'id_status' => function () use ($faker, $statusIds) {
+        //             return $faker->randomElement($statusIds);
+        //         },
+        //         'id_bootcamp' => function () use ($faker, $bootcampIds) {
+        //             return $faker->randomElement($bootcampIds);
+        //         },
+        //     ]);
+
         DB::table('users')->insert([
             'name' => 'Admin',
             'surname' => 'Admin', 
