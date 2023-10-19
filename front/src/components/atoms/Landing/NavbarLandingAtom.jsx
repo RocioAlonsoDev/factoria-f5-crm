db4167142639c4b674c028484b780d2274fa07bb
alfaryolanda79 <alfaroyolanda@hotmail.com>
2023-10-20 01:07:31 +0200

    landing navbar and banner

--- front/src/components/atoms/Landing/NavbarLandingAtom.jsx
+++ front/src/components/atoms/Landing/NavbarLandingAtom.jsx
@@ -13,11 +13,12 @@ export default function NavbarLandingAtom(props) {
       <nav className="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+           
             <Link
-              className="text-white text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+              className="text-orange-500 text-3xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
               to="/"
             >
-              Notus React
+              Factoría F5
             </Link>
             <button
               className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -36,13 +37,13 @@ export default function NavbarLandingAtom(props) {
           >
             <ul className="flex flex-col lg:flex-row list-none mr-auto">
               <li className="flex items-center">
-                <a
+                {/* <a
                   className="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
                   href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus?ref=nr-auth-navbar"
                 >
                   <i className="lg:text-blueGray-200 text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2" />{" "}
                   Docs
-                </a>
+                </a> */}
               </li>
             </ul>
             <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
@@ -84,10 +85,10 @@ export default function NavbarLandingAtom(props) {
 
               <li className="flex items-center">
                 <button
-                  className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+                  className="bg-orange-500 text-white active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                   type="button"
                 >
-                  <i className="fas fa-arrow-alt-circle-down"></i> Download
+                  <i className="fas fa-arrow-alt-circle-down"></i> Login
                 </button>
               </li>
             </ul>
--- front/src/pages/Landing.jsx
+++ front/src/pages/Landing.jsx
@@ -18,7 +18,7 @@ export default function Landing() {
             className="absolute top-0 w-full h-full bg-center bg-cover"
             style={{
               backgroundImage:
-                "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+                "url('https://factoriaf5.org/wp-content/uploads/2021/08/APRENDE-IMAGEN-1.jpg')",
             }}
           >
             <span
@@ -31,12 +31,15 @@ export default function Landing() {
               <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
                 <div className="pr-12">
                   <h1 className="text-white font-semibold text-5xl">
-                    Your story starts with us.
+                  Aprende con Factoría F5, <br />
+                    ¡Haz un cambio en tu vida!
                   </h1>
-                  <p className="mt-4 text-lg text-blueGray-200">
-                    This is a simple example of a Landing Page you can build
-                    using Notus React. It features multiple CSS components based
-                    on the Tailwind CSS design system.
+                  <p className="mt-8 text-2xl text-white">
+                  #RompemosLosCodigos <br /><br />
+
+                   <span className=""> No necesitas titulación o conocimientos previos. <br />
+                    Sólo importa tu motivación, tu lógica, tu autonomía y tu capacidad de trabajar en equipo.</span>
+
                   </p>
                 </div>
               </div>
@@ -63,19 +66,37 @@ export default function Landing() {
           </div>
         </div>
 
-        <section className="pb-20 bg-blueGray-200 -mt-24">
+        <section className="pb-20 bg-blueGray-200 -mt-20">
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap">
               <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
-                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                  <Link to="/recruitment/selectionday/add">
+          <button className="bg-orange-500 border border-orange-500 text-white uppercase font-semibold px-4 py-2 rounded mb-4 ">
+            Inscríbete a FemCoders
+          </button>
+        </Link>
+                    {/* <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-orange-500">
                       <i className="fas fa-award"></i>
-                    </div>
-                    <h6 className="text-xl font-semibold">Awarded Agency</h6>
+                    </div> */}
+                    <img
+                    alt="..."
+                    src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
+                    className="w-full align-middle rounded-t-lg"
+                  />
+                    <h6 className="text-xl font-semibold">FemCoders</h6>
                     <p className="mt-2 mb-4 text-blueGray-500">
-                      Divide details about your product or agency work into
-                      parts. A paragraph describing a feature will be enough.
+                      Bootcamp intensivo Full Stack.
+                        
+                        Inicio:
+                        Octubre 2023.
+                        Duración:
+                        6 meses, 850 horas.
+                        Modalidad:
+                        Online (en tiempo real).
+                        Financiador:
+                        Google.org 
                     </p>
                   </div>
                 </div>
