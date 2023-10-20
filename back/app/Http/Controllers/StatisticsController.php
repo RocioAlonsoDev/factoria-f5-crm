efef3f31a4b6e7757d60f795297ec74a81ddfc60
AmaiiaAbaroa <abaroa.amaia@gmail.com>
2023-10-20 12:40:04 +0200

    getTotalWomenByYear method working back and front

--- back/app/Http/Controllers/StatisticsController.php
+++ back/app/Http/Controllers/StatisticsController.php
@@ -32,20 +32,66 @@ class StatisticsController extends Controller
    }
 
 
+// public function getTotalWomenByYear(): JsonResponse
+// {
+//     // Obtenemos el año actual
+//     $currentYear = now()->year;
+
+//     // Consulta para contar mujeres por año actual
+//     $currentYearWomen = Person::where('gender', 'female')
+//         ->whereYear('created_at', $currentYear)
+//         ->count();
+
+//     // Consulta para contar mujeres por año anterior
+//     $previousYear = $currentYear - 1;
+//     $previousYearWomen = Person::where('gender', 'female')
+//         ->whereYear('created_at', $previousYear)
+//         ->count();
+
+//     // Calculamos los porcentajes
+//     $totalCurrentYearWomen = $currentYearWomen + $previousYearWomen;
+    
+//     // Comprobamos si el total es cero antes de la división
+//     if ($totalCurrentYearWomen === 0) {
+//         $currentYearPercentage = 0;
+//         $previousYearPercentage = 0;
+//     } else {
+//         $currentYearPercentage = ($currentYearWomen / $totalCurrentYearWomen) * 100;
+//         $previousYearPercentage = ($previousYearWomen / $totalCurrentYearWomen) * 100;
+//     }
+
+//     // Calculamos la diferencia en porcentaje entre current_year y previous_year
+//     $percentageDifference = $currentYearPercentage - $previousYearPercentage;
+
+//     return response()->json([
+//         'current_year' => [
+//             'year' => $currentYear,
+//             'total_women' => $currentYearWomen,
+//             'percentage' => $currentYearPercentage,
+//         ],
+//         'previous_year' => [
+//             'year' => $previousYear,
+//             'total_women' => $previousYearWomen,
+//             'percentage' => $previousYearPercentage,
+//         ],
+//         'percentage_difference' => $percentageDifference, // Nueva clave con la diferencia en porcentaje
+//     ], 200);
+// }
+
 public function getTotalWomenByYear(): JsonResponse
 {
     // Obtenemos el año actual
     $currentYear = now()->year;
 
     // Consulta para contar mujeres por año actual
-    $currentYearWomen = Person::where('gender', 'female')
-        ->whereYear('created_at', $currentYear)
+    $currentYearWomen = Person::where('gender', 'Mujer')
+        ->whereRaw('YEAR(created_at) = ?', [$currentYear])
         ->count();
 
     // Consulta para contar mujeres por año anterior
     $previousYear = $currentYear - 1;
-    $previousYearWomen = Person::where('gender', 'female')
-        ->whereYear('created_at', $previousYear)
+    $previousYearWomen = Person::where('gender', 'Mujer')
+        ->whereRaw('YEAR(created_at) = ?', [$previousYear])
         ->count();
 
     // Calculamos los porcentajes
@@ -78,7 +124,6 @@ public function getTotalWomenByYear(): JsonResponse
     ], 200);
 }
 
-   
 
 
    public function getTotalAgePercentages(): JsonResponse
