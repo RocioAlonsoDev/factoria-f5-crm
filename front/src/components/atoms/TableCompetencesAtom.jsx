a6c13cbf547482dbf5ca9dc0974cc5737a886185
NINETTHE <raissa.escobar1408@gmail.com>
2023-10-20 19:39:46 +0200

    Retrieval of data from the categories api and finished skills, but a selector for each skill still needs to be added

--- front/src/components/atoms/TableCompetencesAtom.jsx
+++ front/src/components/atoms/TableCompetencesAtom.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 function TableCompetencesAtom(props) {
   const { categories, captionTittles } = props;
-  console.log(categories);
 
   return (
     <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
@@ -16,37 +15,37 @@ function TableCompetencesAtom(props) {
               <tr>
                 <th className="border-r px-6 py-4 dark:border-neutral-200">Fecha</th>
                 <th className="border-r px-6 py-4 dark:border-neutral-200">Tipo</th>
-                {categories &&
-                  categories.map((category) => (
-                    <th
-                      scope="col"
-                      className="border-r px-6 py-4 dark:border-neutral-200"
-                      key={category.name}
-                      colSpan={category.skills.length}
-                    >
-                      {category.name}
-                    </th>
-                  ))}
+                  {categories &&
+                    categories.map((category) => (
+                      <th
+                        scope="col"
+                        className="border-r px-6 py-4 dark:border-neutral-200"
+                        key={category.name}
+                        colSpan={category.skills.length}>
+                        {category.name}
+                      </th>
+                    ))}
               </tr>
             </thead>
-
             <tbody className="border-b font-medium dark:border-neutral-200">
-  <tr>
-    <td className="border-r px-6 py-4 dark:border-neutral-200">Fecha</td>
-    <td className="border-r px-6 py-4 dark:border-neutral-200">Tipo</td>
-    {categories.map((category) =>
-      category.skills.map((skill, skillIndex) => (
-        <td
-          key={`${category.name}-${skillIndex}`}
-          className="border-r px-6 py-4 dark:border-neutral-200"
-        >
-          {skill.name}
-        </td>
-      ))
-    )}
-  </tr>
-</tbody>
-
+              <tr>
+                <td className="border-r px-6 py-4 dark:border-neutral-200"></td>
+                <td className="border-r px-6 py-4 dark:border-neutral-200"></td>
+                  {categories.map((category) =>
+                    category.skills.length ? (
+                      category.skills.map((skill, skillIndex) => (
+                        <td
+                          key={`${category.name}-${skillIndex}`}
+                          className="border-r px-6 py-4 dark:border-neutral-200">
+                          {skill.name}
+                        </td>
+                      ))
+                  ) : (
+                <td className="border-r px-6 py-4 dark:border-neutral-200">-</td>
+                  )
+                )}
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
