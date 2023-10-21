e03c6465d94de108cbc154bfa6a7c50a8723577c
SoleGrobas <solegrobas@yahoo.com.ar>
2023-10-21 12:36:46 +0200

    fixing details to push

--- front/src/pages/PersonBootcamp/PeoplebyBootcamp.jsx
+++ front/src/pages/PersonBootcamp/PeoplebyBootcamp.jsx
@@ -2,7 +2,6 @@
 import BootcampDataService from "../../services/crmService/bootcamp.service";
 import { useState, useEffect } from "react";
 import { useParams } from 'react-router';
-import {Link} from 'react-router-dom';
 
 export default function PeoplebyBootcamp () {
     const [bootcamp, setBootcamp] = useState({
@@ -45,39 +44,45 @@ export default function PeoplebyBootcamp () {
             
                 <div className="text-center flex justify-between">
               
-                    <h6 className="text-blueGray-700 text-xl font-bold">Apirantes {bootcamp.bootcampData.name}</h6>
-                    <h6 className="text-blueGray-700 text-xl font-bold">Inicio {bootcamp.bootcampData.startDate}</h6>
-                    <h6 className="text-blueGray-700 text-xl font-bold">Finalización {bootcamp.bootcampData.endDate}</h6>
+                    <h6 className="text-blueGray-700 text-xl font-bold">Apirantes por Bootcamp</h6>
+                   
                 </div>
             </div>
             <div>
             <table className="items-center w-full bg-transparent border-collapse">
             <thead>
               <tr>
-                
                 <th
                   className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-bold text-left "
-                    
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " 
                   }
                 >
-                  Nombre Aspirante
+                  Id Bootcamp
                 </th>
                 <th
                   className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-bold text-left "
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " 
+                    
                     
                   }
                 >
-                  Apellido Aspirante
+                  Nombre Bootcamp
                 </th>
                 <th
                   className={
-                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-bold text-left "
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left "
                     
                   }
                 >
-                  ' '
+                  Inicio Bootcamp
+                </th>
+                <th
+                  className={
+                    "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left "
+                    
+                  }
+                >
+                  Aspirantes
                 </th>
                 </tr>
                 </thead>
@@ -85,26 +90,21 @@ export default function PeoplebyBootcamp () {
                 {bootcamp.peopleInBootcamp.map((person) => (
                   <tr key={person.id}>
                 
-                
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-s whitespace-nowrap p-4">
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-circle text-emerald-500 mr-2"></i>{" "}
+                  {bootcamp.bootcampData.id}
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                {bootcamp.bootcampData.name}
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                  <i className="fas fa-circle text-emerald-500 mr-2"></i>{" "}
+                  {bootcamp.bootcampData.startDate} 
+                </td>
+                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                   <i className="fas fa-circle text-emerald-500 mr-2"></i>{" "}
                   {person.name}
                 </td>
-                <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-s whitespace-nowrap p-4">
-                  <i className="fas fa-circle text-emerald-500 mr-2"></i>{" "}
-                  {person.surname}
-                </td>
-                <td>
-                <Link to={`/person/${person.id}`}>
-                  <button
-                             className="bg-orange-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
-                             type="button"
-                          
-                    >
-                    Ficha Aspirante
-                  </button>
-                  </Link>
-                  </td>
                 </tr>
                   ))}
                 </tbody>
