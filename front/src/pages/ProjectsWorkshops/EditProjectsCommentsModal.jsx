575314178d39bd4d37f2b0d97ee217a870de83c0
BeatrizCano <beatrizcafernandez@gmail.com>
2023-10-22 15:33:59 +0200

    change_styles_edit_projects

--- front/src/pages/ProjectsWorkshops/EditProjectsCommentsModal.jsx
+++ front/src/pages/ProjectsWorkshops/EditProjectsCommentsModal.jsx
@@ -45,10 +45,10 @@ export default function EditProjectsCommentsModal({ setIsEditModalOpen, commentI
 
   return (
     <>
-    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-      <div className="relative w-auto my-6 mx-auto max-w-3xl">
-        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-          <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+    <div className="relative flex flex-col md:top-[107px] min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded py-12 pl-64 pb-28">
+      <div className="rounded-t mb-0 px-4 py-3 border-0">
+        <div className="border-0 rounded-lg relative flex flex-col w-full outline-none focus:outline-none">
+          <div className="flex items-start justify-between p-5 border-b border-blueGray-200 rounded-t">
             <h3 className="text-3xl font-semibold">Editar Comentario</h3>
             <button
               className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
