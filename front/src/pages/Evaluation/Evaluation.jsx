9d079822e15f68e9c85ea9ea3dd31f135909db32
NINETTHE <raissa.escobar1408@gmail.com>
2023-10-23 00:10:42 +0200

    Filter by skills and categories according to stack but it is not finished

--- front/src/pages/Evaluation/Evaluation.jsx
+++ front/src/pages/Evaluation/Evaluation.jsx
@@ -2,17 +2,18 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import TableContentAtom from '../../components/atoms/TableContentAtom';
 import TableCompetencesAtom from '../../components/atoms/TableCompetencesAtom';
-import CategoryDataService from '../../services/trackingService/category.service';
-import BootcampStackDataService from '../../services/trackingService/bootcampStack.service';
+import skillsService from '../../services/trackingService/skills.service';
 import personService from '../../services/crmService/person.service';
 import bootcampStackService from '../../services/trackingService/bootcampStack.service';
+import CategoryDataService from '../../services/trackingService/category.service'
 
 
 export default function Evaluation() {
   const {id} = useParams();
-  const [categories, setCategories] = useState([])
+  // const [categories, setCategories] = useState([])
   const [isLoading, setIsLoading] = useState(true);
   const [stacks, setStacks]= useState([]);
+  const [categories, setCategories] = useState([]);
   const [person, setPerson] = useState([]);
   const [bootcampName, setBootcampName] = useState('');
 
@@ -33,58 +34,69 @@ export default function Evaluation() {
   // }, []);
 
 
-  // useEffect(() => {
-  //   const fetchStack = async () => {
-  //     try {
-  //       const response = await BootcampStackDataService.get(id);
-  //       setStacks(response.data);
-  //       setIsLoading(false);
-  //     } catch (error) {
-  //       console.log(error);
-  //       setIsLoading(false);
-  //     }
-  //   };
-  //   fetchStack();
-  // }, [id]);
 
   
-    useEffect(() => {
-      const fetchPersonDetails = async () => {
-        try {
-          const response = await personService.get(id);
-          const personData = response.data;
-  
-          setPerson(personData);
-  
-          if (personData.bootcamp) {
-            const bootcampId = personData.bootcamp.id;
-            setBootcampName(personData.bootcamp.name);
-  
-            // Obtener los stacks asociados al bootcamp dinámicamente
-            const stackResponse = await bootcampStackService.getAll();
-            const stackData = stackResponse.data;
-  
-            if (Array.isArray(stackData)) {
-              // Filtra los stacks que coinciden con el bootcampId
-              const filteredStacks = stackData.filter(stack => stack.bootcamp_id === bootcampId);
-  
-              // Obtiene los nombres de los stacks filtrados
-              const stackNames = filteredStacks.map(stack => stack.stacks[0].name);
-              console.log(stackNames)
-              setStacks(stackNames);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const personResponse = await personService.get(id);
+        const personData = personResponse.data;
+
+        setPerson(personData);
+
+        if (personData.bootcamp) {
+          const bootcampId = personData.bootcamp.id;
+          setBootcampName(personData.bootcamp.name);
+
+          // Obtener los stacks asociados al bootcamp dinámicamente
+          const stackResponse = await bootcampStackService.getAll();
+          const stackData = stackResponse.data;
+
+          if (Array.isArray(stackData)) {
+            const filteredStacks = stackData.filter(
+              (stack) => stack.bootcamp_id === bootcampId
+            );
+
+            const stackNames = filteredStacks.map(
+              (stack) => stack.stacks[0].name
+            );
+
+            // Almacenar los nombres de las stacks
+            setStacks(stackNames);
+
+            // Obtener todos los datos de skills desde la API
+            const skillsResponse = await CategoryDataService.getAll();
+            const skillsData = skillsResponse.data;
+
+            if (Array.isArray(skillsData)) {
+              // Filtra los skills que están asociados a algún stack en bootcampStack
+              const filteredSkills = skillsData.filter((skill) =>
+                filteredStacks.some(
+                  (stack) => stack.stacks[0].skill_id === skill.id
+                )
+              );
+              console.log(filteredSkills)
+
+              // Almacena las skills asociadas a las stacks
+              setCategories(filteredSkills);
             } else {
-              console.error('Los datos de la API no contienen información de stacks válida.');
+              console.error('Los datos de la API no contienen información de skills válida.');
             }
           } else {
-            console.error('Los datos de la persona no contienen información de bootcamp.');
+            console.error('Los datos de la API no contienen información de stacks válida.');
           }
-        } catch (error) {
-          console.error(error);
+        } else {
+          console.error('Los datos de la persona no contienen información de bootcamp.');
         }
-      };
-  
-      fetchPersonDetails();
-    }, [id]);
+
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
 
   // Estado para controlar si los selectores deben mostrarse
   const [showSelects, setShowSelects] = useState(false);
@@ -113,9 +125,9 @@ export default function Evaluation() {
     <div className='md:block md:absolute md:top-16 md:left-64 md:right-0 w-auto p-2'>
       <div>
       <h1>Formulario de Evaluación</h1>
-      <p>Persona: {person.name}</p>
+      <p>Coder: {person.name}</p>
     </div>
-      {/* <div>
+      <div>
         {isLoading ? (
           <p>Cargando...</p>
         ) : (
@@ -123,10 +135,11 @@ export default function Evaluation() {
             <TableCompetencesAtom
               captionTittles="Evolución de competencias()"
               categories={categories}
+
             />
           </div>
         )}
-      </div> */}
+      </div>
 
       <div>
         <TableContentAtom
