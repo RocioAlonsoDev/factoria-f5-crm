fea2e3818509d8ab54764eff30ebacf4a021a73d
NINETTHE <raissa.escobar1408@gmail.com>
2023-10-22 18:28:09 +0200

    Connection to apiPerson set filter by id of the person shows the bootcamp the coder belongs to

--- front/src/pages/Evaluation/Evaluation.jsx
+++ front/src/pages/Evaluation/Evaluation.jsx
@@ -1,30 +1,82 @@
 import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import TableContentAtom from '../../components/atoms/TableContentAtom';
 import TableCompetencesAtom from '../../components/atoms/TableCompetencesAtom';
 import CategoryDataService from '../../services/trackingService/category.service';
 import BootcampStackDataService from '../../services/trackingService/bootcampStack.service';
+import personService from '../../services/crmService/person.service';
+import bootcampStackService from '../../services/trackingService/bootcampStack.service';
+
 
 export default function Evaluation() {
+  const {id} = useParams();
   const [categories, setCategories] = useState([])
   const [isLoading, setIsLoading] = useState(true);
   const [stacks, setStacks]= useState([]);
+  const [person, setPerson] = useState([]);
+  const [bootcampName, setBootcampName] = useState('');
+
+  // useEffect(() => {
+  //   const fetchCategory = async () => {
+  //     try {
+  //       const response = await CategoryDataService.getAll();
+      
+  //       setCategories(response.data);
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       console.log(error);
+  //       setIsLoading(false); 
+  //     }
+  //   };
+
+  //   fetchCategory();
+  // }, []);
+
+
+  // useEffect(() => {
+  //   const fetchStack = async () => {
+  //     try {
+  //       const response = await BootcampStackDataService.get(id);
+  //       setStacks(response.data);
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       console.log(error);
+  //       setIsLoading(false);
+  //     }
+  //   };
+  //   fetchStack();
+  // }, []);
 
   useEffect(() => {
-    const fetchCategory = async () => {
+    const fetchPersonDetails = async () => {
       try {
-        const response = await CategoryDataService.getAll();
-      
-        setCategories(response.data);
+        // Realiza una solicitud a la API para obtener los detalles de la persona
+        const response = await personService.get(id);
+        console.log('Respuesta de la API:', response.data);
+        const personData = response.data;
+
+        // Establece los detalles de la persona en el estado
+        setPerson(personData);
+
+        if (personData && personData.bootcamp) {
+          // Establece los detalles de la persona en el estado
+          setPerson(personData);
+
+          // Accede al nombre del bootcamp desde personData.bootcamp.name
+          const bootcampName = personData.bootcamp.name;
+          setBootcampName(bootcampName);
+        } else {
+          console.error('Los datos de la persona no contienen información de bootcamp.');
+        }
         setIsLoading(false);
       } catch (error) {
-        console.log(error);
-        setIsLoading(false); 
+        console.error( error);
+        setIsLoading(false);
       }
     };
 
-    fetchCategory();
-  }, []);
-
+    fetchPersonDetails();
+  }, [id]);
 
 
 useEffect(() => {
@@ -72,6 +124,10 @@ useEffect(() => {
   return (
     <div className='md:block md:absolute md:top-16 md:left-64 md:right-0 w-auto p-2'>
       <div>
+      <h1>Formulario de Evaluación</h1>
+      <p>Persona: {person.name}</p>
+    </div>
+      {/* <div>
         {isLoading ? (
           <p>Cargando...</p>
         ) : (
@@ -82,12 +138,12 @@ useEffect(() => {
             />
           </div>
         )}
-      </div>
+      </div> */}
 
       <div>
         <TableContentAtom
           date="FECHA"
-          captionTittles="Stacks Femcoders Norte"
+          captionTittles={bootcampName}
           stacks={stacks}
           showSelects={showSelects} // Pasa el estado como prop
         ></TableContentAtom>
