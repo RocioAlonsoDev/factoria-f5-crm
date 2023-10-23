0b25b5816ef499fd25a493634720a629a8092808
SoleGrobas <solegrobas@yahoo.com.ar>
2023-10-23 10:55:03 +0200

    RequirementModal fixed and PeopleBootcamp route to person/id fixed

--- front/src/pages/PersonBootcamp/PeoplebyBootcamp.jsx
+++ front/src/pages/PersonBootcamp/PeoplebyBootcamp.jsx
@@ -95,7 +95,7 @@ export default function PeoplebyBootcamp () {
                   {person.surname}
                 </td>
                 <td>
-                <Link to={`/person/${person.id}`}>
+                <Link to={`/recruitment/person/${person.id}`}>
                   <button
                              className="bg-orange-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
                              type="button"
